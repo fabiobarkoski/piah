ff84beb2c161b3995bd99be452d4d9f85c226b10
fabiobarkoski <fabiobarkoskii@gmail.com>
2024-04-01 01:46:18 -0300

    fix tests and add dependency in pyproject.toml

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,12 +6,14 @@ import pytest
 
 from piah import Piah
 
-AI_KEY = os.getenv("OPENAI_API_KEY") == None
+AI_KEY = os.getenv("OPENAI_API_KEY") is None
+
 
 @dataclass
 class Person1:
     name: str
     age: int
+
 
 @dataclass
 class Person2:
@@ -21,16 +23,21 @@ class Person2:
     color: str
     born_year: int
 
+
 @pytest.mark.skipif(AI_KEY, reason="Key not configured in enrivonment")
 def test_parser_when_receive_custom_system_context():
     to_compare = Person1("python", 33)
-    parser = Piah("gpt-3.5-turbo", "You are a principal software developer who extracts the needed values from text. "
-            "Please supplement the provided JSON object with an "
-            "exact value from the given text. "
-            "You must always extract only the value. "
-            "Your output must be a valid JSON object")
+    parser = Piah(
+        "gpt-3.5-turbo",
+        "You are a principal software developer who extracts the needed values from text. "
+        "Please supplement the provided JSON object with an "
+        "exact value from the given text. "
+        "You must always extract only the value. "
+        "Your output must be a valid JSON object",
+    )
     result = parser.parse("Hello I am python and I am 33 years old", Person1)
     assert result == to_compare
+
 
 @pytest.mark.skipif(AI_KEY, reason="Key not configured in enrivonment")
 def test_parser_when_receive_text():
@@ -39,21 +46,26 @@ def test_parser_when_receive_text():
     result = parser.parse("Hello I am python and I am 33 years old", Person1)
     assert result == to_compare
 
+
 @pytest.mark.skipif(AI_KEY, reason="Key not configured in enrivonment")
 @pytest.mark.xfail
 def test_parser_when_receive_string_pdf():
-    to_compare = Person2("python", 33, "221B Baker Street, London", "green", 1991)
+    to_compare = Person2(
+        "python", 33, "221B Baker Street, London", "green", 1991
+    )
     parser = Piah("gpt-3.5-turbo")
     result = parser.parse("test1.pdf", Person2)
     print(result)
     assert result == to_compare
 
+
 @pytest.mark.skipif(AI_KEY, reason="Key not configured in enrivonment")
 @pytest.mark.xfail
 def test_parser_when_receive_path_pdf():
-    to_compare = Person2("python", 33, "221B Baker Street, London", "green", 1991)
+    to_compare = Person2(
+        "python", 33, "221B Baker Street, London", "green", 1991
+    )
     parser = Piah("gpt-3.5-turbo")
     result = parser.parse(Path("tests/test1.pdf"), Person2)
     print(result)
     assert result == to_compare
-
